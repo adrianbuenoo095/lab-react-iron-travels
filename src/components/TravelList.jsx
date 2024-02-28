74b0c8540489c3371002db365515508d88e096f1
Prabeshika <prabeshika14@gmail.com>
2024-02-28 16:47:50 +0100

    added conditional rendering

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,25 +1,48 @@
-import travelPlansDate from "../assets/travel-plans.json";
-import classes from "./travelList.module.css";
-const TravelList = () => {
-  console.log(travelPlansDate);
+import { useState } from "react";
+import travelData from "../assets/travel-plans.json";
 
-  return travelPlansDate.map((travel) => {
-    return (
-      <div className={classes.container} key={travel.id}>
-        <div>
-          <img className={classes.img} src={travel.image} />
+const TravelList = () => {
+  const [travelPlans, setTravelPlans] = useState(travelData);
+  const handleDelete = (id) => {
+    const updatedPlans = travelPlans.filter((plan) => plan.id !== id);
+    setTravelPlans(updatedPlans);
+  };
+
+  const getlabel = (plan) => {
+    const cost = plan.totalCost;
+    const allInclusive = plan.allInclusive;
+
+    if (cost <= 350) {
+      return "Great Deal" + (allInclusive ? ", All Inclusive" : "");
+    } else if (cost >= 1500) {
+      return "Premium" + (allInclusive ? ", All Inclusive" : "");
+    } else {
+      return allInclusive ? "All Inclusive" : "";
+    }
+  };
+
+  return (
+    <div>
+      {travelPlans.map((plan) => (
+        <div className="container" key={plan.id}>
+          <div>
+            <img className="img" src={plan.image} alt={plan.destination} />
+          </div>
+          <div>
+            <h3 className="destinationName">{plan.destination}</h3>
+            <p>{plan.description}</p>
+            <p>Duration: {plan.days} days</p>
+            <p>
+              <span className="boldText">Price:</span> ${plan.totalCost} (
+              {getlabel(plan)})
+            </p>
+          </div>
+
+          <button onClick={() => handleDelete(plan.id)}>Delete</button>
         </div>
-        <div className={classes.destinationName}>
-          <h1>{travel.destination}</h1>
-          <p>{travel.description}</p>
-          <p>
-            <span className={classes.boldText}>Price:</span>
-            {travel.totalCost}
-          </p>
-        </div>
-      </div>
-    );
-  });
+      ))}
+    </div>
+  );
 };
 
 export default TravelList;
